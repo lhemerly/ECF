4c001844ea617c3019dd945399b76bd9a52e3e48
Luiz Hemerly <lechemerly@gmail.com>
2020-09-20 16:19:02 -0300

    Models Refinement

--- apps/accounting/models.py
+++ apps/accounting/models.py
@@ -2,26 +2,36 @@ from django.db import models
 
 # Create your models here.
 class BalanceSheet(models.Model):
-    Company = models.CharField()
+    CompanyCode = models.CharField(max_length=3)
+    CompanyName = models.CharField(max_length=500)
     Date = models.DateField()
-    AccountCode = models.CharField()
-    AccountName = models.CharField()
-    CostCenterCode = models.CharField()
-    CostCenterName= models.CharField()
-    AccountBalance = models.DecimalField()
+    AccountCode = models.CharField(max_length=500)
+    AccountName = models.CharField(max_length=500)
+    CostCenterCode = models.CharField(max_length=500)
+    CostCenterName= models.CharField(max_length=500)
+    AccountBalance = models.DecimalField(max_length=500,max_digits=12,decimal_places=2)
+    AccountIndicator = models.CharField(max_length=1,
+                                        default= 'D',
+                                        choices=[('D', 'Débito'),
+                                                ('C', 'Crédito')])
 
 class AccountingChart(models.Model):
-    AccountCode = models.CharField()
-    AccountName = models.CharField()
-    CostCenterCode = models.CharField()
-    CostCenterName = models.CharField()
-    AccountLevel1 = models.CharField() #Assets, Liabilities, Equity or Income Statement
-    AccountLevel2 = models.CharField() #Short-Term, Long-Term, Equity, Income Statement
-    AccountLevel3 = models.CharField()
-    AccountLevel4 = models.CharField()
-    AccountLevel5 = models.CharField()
-    AccountLevel6 = models.CharField()
-    AccountLevel7 = models.CharField()
-    ECDAccountCode = models.CharField()
-    ECDAccountName = models.CharField()
-    ECDAccountGroup = models.CharField()
+    AccountCode = models.CharField(max_length=500)
+    AccountName = models.CharField(max_length=500)
+    CostCenterCode = models.CharField(max_length=500)
+    CostCenterName = models.CharField(max_length=500)
+    AccountLevel1 = models.CharField(max_length=500) #Assets, Liabilities, Equity or Income Statement
+    AccountLevel2 = models.CharField(max_length=500) #Short-Term, Long-Term, Equity, Income Statement
+    AccountLevel3 = models.CharField(max_length=500)
+    AccountLevel4 = models.CharField(max_length=500)
+    AccountLevel5 = models.CharField(max_length=500)
+    AccountLevel6 = models.CharField(max_length=500)
+    AccountLevel7 = models.CharField(max_length=500)
+    ECDAccountCode = models.CharField(max_length=500)
+    ECDAccountName = models.CharField(max_length=500)
+    ECDAccountGroup = models.CharField(max_length=500)
+
+class Company(models.Model):
+    CompanyCode = models.CharField(max_length=3)
+    CompanyName = models.CharField(max_length=500)
+    CNPJ = models.CharField(max_length=14)  #TODO: CNPJ VALIDATOR
--- apps/blockM/models.py
+++ apps/blockM/models.py
@@ -4,54 +4,73 @@ from django.db import models
 
 
 class BlockM001(models.Model):
-    BlockName = models.CharField(max_length=4, default="M001")  #TODO: FIXED VALUE FIELD
+    BlockName = models.CharField(max_length=4,
+                                 default="M001", editable=False)
     Informed = models.BooleanField()
 
 
-class BlockM010(models.Model): #TODO: VALIDATOR -> MANDATORY FIELDS
-    BlockName = models.CharField(max_length=4, default="M010")  #TODO: FIXED VALUE FIELD
-    BAccountCode = models.CharField()
-    BAccountDesc = models.CharField()
+class BlockM010(models.Model):
+    BlockName = models.CharField(max_length=4,
+                                 default="M010", editable=False)
+    BAccountCode = models.CharField(max_length=500)
+    BAccountDesc = models.CharField(max_length=500)
     AccountCreatedDate = models.DateField(max_length=8)  #TODO: VALIDATOR-> LOWER THAN FINAL DATE
     DefaultTableCode = models.CharField(max_length=6)  #TODO: VALIDATOR CODE OF THE DEFAULT TABLE SEE ECF MANUAL
-    LimitDate = models.DateField(max_length=8)  #NOT MANDATORY
-    TaxCode = models.CharField(max_length=1)  #TODO: VALIDATOR(ONLY ACCEPTS [I;C])
-    InitialBalance = models.DecimalField(max_length=19, max_digits=2)
-    InitialBalanceInd = models.CharField(max_length=1)  #TODO: VALIDATOR(ONLY ACCEPTS [D;C]
+    LimitDate = models.DateField(max_length=8,
+                                 blank=True)
+    TaxCode = models.CharField(max_length=1,
+                               choices=[('I', 'IR'),
+                                        ('C', 'CSLL')])
+    InitialBalance = models.DecimalField(max_length=19, max_digits=12,decimal_places=2)
+    InitialBalanceInd = models.CharField(max_length=1,
+                                         choices=[('D', 'Débito'),
+                                                  ('C', 'Crédito')])
     CNPJEspSituation = models.CharField(max_length=14)  #TODO: CNPJ VALIDATOR
 
 
 class BlockM030(models.Model):
-    BlockName = models.CharField(max_length=4, default="M030")  #TODO: FIXED VALUE FIELD
+    BlockName = models.CharField(max_length=4, default="M030", editable=False)
     InitialDate = models.DateField(max_length=8)
     FinalDate = models.DateField(max_length=8)
 
 
 class BlockM300(models.Model):
-    BlockName = models.CharField(max_length=4, default="M300")  #TODO: FIXED VALUE FIELD
+    BlockName = models.CharField(max_length=4, default="M300", editable=False)
     LalurCode = models.IntegerField()  #TODO: ENUMERATE OPTIONS FROM ECF DEFAULT TABLE
-    LalurName = models.CharField()  #TODO: FROM LALUR CODE
-    AdjustmentType = models.CharField(max_length=1)  #TODO: VALIDATOR ONLY [A;E;P;L]
-    RelationType = models.CharField(max_length=1)  #TODO: VALIDATOR ONLY [1;2;3;4]
-    Value = models.DecimalField(max_length=19, max_digits=2)
+    LalurName = models.CharField(max_length=500)  #TODO: FROM LALUR CODE
+    AdjustmentType = models.CharField(max_length=1,
+                                      choices=[('A', 'Adição'),
+                                               ('E', 'Exclusão'),
+                                               ('P', 'Compensação de Prejuízo'),
+                                               ('L', 'Lucro')])
+    RelationType = models.CharField(max_length=1,
+                                    choices=[('1', 'Com conta da parte B'),
+                                             ('2', 'Com conta contábil'),
+                                             ('3', 'Com conta da parte B e conta contábil'),
+                                             ('4', 'Sem relacionamento')])
+    Value = models.DecimalField(max_digits=12,decimal_places=2)
     Comments = models.CharField(max_length=500)
 
 
 class BlockM305(models.Model):
-    BlockName = models.CharField(max_length=4, default="M305")  # TODO: FIXED VALUE FIELD
-    BAccountCode = models.CharField()
-    Value = models.DecimalField(max_length=19, max_digits=2)
-    ValueInd = models.CharField(max_length=1)  # TODO: VALIDATOR(ONLY ACCEPTS [D;C]
+    BlockName = models.CharField(max_length=4, default="M305", editable=False)
+    BAccountCode = models.CharField(max_length=500)
+    Value = models.DecimalField(max_digits=12,decimal_places=2)
+    ValueInd = models.CharField(max_length=1,
+                                choices=[('D', 'Débito'),
+                                        ('C', 'Crédito')])
     LalurCode = models.IntegerField()  #TODO: ENUMERATE OPTIONS FROM ECF DEFAULT TABLE
-    LalurName = models.CharField()  #TODO: FROM LALUR CODE
-    AccountCode = models.CharField()  #From Accounting App - Non-mandatory
-    CostCenterCode = models.CharField()  #From Accounting App - Non-mandatory
+    LalurName = models.CharField(max_length=500)  #TODO: FROM LALUR CODE
+    AccountCode = models.CharField(max_length=500, blank=True)  #From Accounting App - Non-mandatory
+    CostCenterCode = models.CharField(max_length=500, blank=True)  #From Accounting App - Non-mandatory
 
 
 class BlockM310(models.Model):
-    BlockName = models.CharField(max_length=4, default="M310")  # TODO: FIXED VALUE FIELD
-    AccountCode = models.CharField()  # From Accounting App
-    CostCenterCode = models.CharField()  # From Accounting App
-    Value = models.DecimalField(max_length=19, max_digits=2)
-    ValueInd = models.CharField(max_length=1)  # TODO: VALIDATOR(ONLY ACCEPTS [D;C]
+    BlockName = models.CharField(max_length=4, default="M310", editable=False)
+    AccountCode = models.CharField(max_length=500)  # From Accounting App
+    CostCenterCode = models.CharField(max_length=500)  # From Accounting App
+    Value = models.DecimalField(max_digits=12,decimal_places=2)
+    ValueInd = models.CharField(max_length=1,
+                                choices=[('D', 'Débito'),
+                                        ('C', 'Crédito')])
 
